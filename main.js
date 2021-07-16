d87a3400eaf096d7a6c38d896e8257569332ffe1
Ray Pearcy <qaray4791@gmail.com>
2021-07-15 19:41:59 -0500

    just a bit of cleanup and not worrying about multiple rooms or spawns yet

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 module.exports.loop = function () {
 
-    // Let's figure out the room name(s) and Room Controller Level of each
-    for (var room in Game.rooms) {
+    // Pick a room and place your spawn down. This is done manually for now.
+
+    // Once you have placed a spwn, we figure out the Room Name, Room Controller Level, and Spawn Name
+    for (let room in Game.rooms) {
         var roomName = Game.rooms[room];
         var roomControllerLevel = (roomName.controller.level);
+    }
+    
+    var spawns = roomName.find(FIND_MY_SPAWNS, {
+        filter: (structure) => {
+            return (structure.structureType == STRUCTURE_SPAWN);
+        }
+    });
 
-        // We also want to know the spawn in the room
-        var spawns = roomName.find(FIND_MY_SPAWNS, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_SPAWN);
-            }
-        });
-
-        for (let spawn of spawns) {
-            var spawnName = spawn.name;
-            console.log('Room Name: ' + roomName.name + ' -- RCL: ' + roomControllerLevel + ' -- Spawn Name: ' + spawnName);
-        }        
+    for (let spawn of spawns) {
+        var spawnName = spawn.name;
+        console.log('Room Name: ' + roomName.name + ' -- RCL: ' + roomControllerLevel + ' -- Spawn Name: ' + spawnName);
     }
 }
