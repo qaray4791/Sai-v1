26a6712f5347ed3b8f0ed205bbdca8b04f608901
Ray Pearcy <qaray4791@gmail.com>
2021-07-15 19:58:53 -0500

    refactor and added memory clearing for dead creeps

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ module.exports.loop = function () {
 
     // Pick a room and place your spawn down. This is done manually for now.
 
-    // Once you have placed a spwn, we figure out the Room Name, Room Controller Level, and Spawn Name
+    // Once you have placed a spawn, we figure out the Room Name, Room Controller Level, and Spawn Name
     for (let room in Game.rooms) {
         var roomName = Game.rooms[room];
         var roomControllerLevel = (roomName.controller.level);
@@ -17,5 +17,15 @@ module.exports.loop = function () {
     for (let spawn of spawns) {
         var spawnName = spawn.name;
         console.log('Room Name: ' + roomName.name + ' -- RCL: ' + roomControllerLevel + ' -- Spawn Name: ' + spawnName);
+    }
+
+    // Clear dead creeps from memory
+    if (Game.time % 100 == 0) {
+        for (var name in Memory.creeps) {
+            if (!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
+        }
     }
 }
